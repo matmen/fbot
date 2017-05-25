43cb45198c9b5a2a8f7378115ceabfc87df43af6
matmen <gcmatmen@gmail.com>
2017-05-25 21:48:25 +0200

    Fix error function in ascii command

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -8,7 +8,7 @@ module.exports = {
 		if(!argsString) return this.commandHandler.invalidArguments(message);
 
 		this.figlet(this.utils.filterMentions(argsString), function(err, data) {
-			if(err) return this.utils.handleCommandError(err);
+			if(err) return this.utils.handleCommandError(err, message);
 			if(data.length > 2000) return message.channel.send('The message you tried to convert is too long, try something shorter');
 
 			message.channel.send(data, {
