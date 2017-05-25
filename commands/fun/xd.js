2d5e0e5033ffa09de6c38f9e4ed78ed716f6aa13
matmen <gcmatmen@gmail.com>
2017-05-25 18:07:48 +0200

    Reformat code, use const, fix jslint issues

--- commands/fun/xd.js
+++ commands/fun/xd.js
@@ -9,21 +9,20 @@ module.exports = {
 
 		let xd = [this.utils.filterMentions(args[0].substring(0, 16)), this.utils.filterMentions(args[1 % args.length].substring(0, 16)), this.utils.filterMentions(args[2 % args.length].substring(0, 16))];
 
-		const s = (x) => String.prototype.repeat.call(' ', x)
+		const s = (x) => String.prototype.repeat.call(' ', x);
 
-		let a = xd[0],
+		const a = xd[0],
 			b = xd[1],
 			c = xd[2],
-			d = ' \n'
+			d = ' \n';
 
-		let reply = 
-			       a + s(11) + a + s(5) + b + s(2) + c + d +
-			s(2) + a + s(7)  + a + s(7) + b + s(5) + c + d +
-			s(4) + a + s(3)  + a + s(9) + b + s(6) + c + d +
+		let reply = a + s(11) + a + s(5) + b + s(2) + c + d +
+			s(2) + a + s(7) + a + s(7) + b + s(5) + c + d +
+			s(4) + a + s(3) + a + s(9) + b + s(6) + c + d +
 			s(6) + a + s(11) + s(Math.round(a.length / 2 - 1)) + b + s(6) + c + d +
-			s(4) + a + s(3)  + a + s(9) + b + s(6) + c + d +
-			s(2) + a + s(7)  + a + s(7) + b + s(5) + c + d +
-				   a + s(11) + a + s(5) + b + s(2) + c;
+			s(4) + a + s(3) + a + s(9) + b + s(6) + c + d +
+			s(2) + a + s(7) + a + s(7) + b + s(5) + c + d +
+			a + s(11) + a + s(5) + b + s(2) + c;
 
 		message.channel.send(reply, {
 			code: true
