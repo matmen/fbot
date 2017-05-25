985f80672976b2e7559bf21659417a8abcf4a6ce
Arze (OSX) <ezraripps@Ezras-MBP.fios-router.home>
2017-05-25 12:02:54 -0400

    rewrite xd.js

--- commands/fun/xd.js
+++ commands/fun/xd.js
@@ -9,13 +9,21 @@ module.exports = {
 
 		let xd = [this.utils.filterMentions(args[0].substring(0, 16)), this.utils.filterMentions(args[1 % args.length].substring(0, 16)), this.utils.filterMentions(args[2 % args.length].substring(0, 16))];
 
-		let reply = xd[0] + '           ' + xd[0] + '     ' + xd[1] + '  ' + xd[2] + ' \n' +
-			'  ' + xd[0] + '       ' + xd[0] + '       ' + xd[1] + '     ' + xd[2] + ' \n' +
-			'    ' + xd[0] + '   ' + xd[0] + '         ' + xd[1] + '      ' + xd[2] + ' \n' +
-			'       ' + xd[0] + '           ' + ' '.repeat(Math.round(xd[0].length / 2 - 1)) + xd[1] + '      ' + xd[2] + ' \n' +
-			'    ' + xd[0] + '   ' + xd[0] + '         ' + xd[1] + '      ' + xd[2] + ' \n' +
-			'  ' + xd[0] + '       ' + xd[0] + '       ' + xd[1] + '     ' + xd[2] + ' \n' +
-			xd[0] + '           ' + xd[0] + '     ' + xd[1] + '  ' + xd[2];
+		const s = (x) => String.prototype.repeat.call(' ', x)
+
+		let a = xd[0],
+			b = xd[1],
+			c = xd[2],
+			d = ' \n'
+
+		let reply = 
+			       a + s(11) + a + s(5) + b + s(2) + c + d +
+			s(2) + a + s(7)  + a + s(7) + b + s(5) + c + d +
+			s(4) + a + s(3)  + a + s(9) + b + s(6) + c + d +
+			s(7) + a + s(11) + s(Math.round(a.length / 2 - 1)) + b + s(6) + c + d +
+			s(4) + a + s(3)  + a + s(9) + b + s(6) + c + d +
+			s(2) + a + s(7)  + a + s(7) + b + s(5) + c + d +
+				   a + s(11) + a + s(5) + b + s(2) + c;
 
 		message.channel.send(reply, {
 			code: true
