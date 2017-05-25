86a1a695b0aca7f2754535152664870b42b24b87
matmen <gcmatmen@gmail.com>
2017-05-25 18:18:11 +0200

    Fix calls without image not send the invalid arguments message

--- commands/image-manipulation/gay.js
+++ commands/image-manipulation/gay.js
@@ -1,10 +1,15 @@
 module.exports = {
 	description: 'Adds the gay pride to the given argument',
+	args: '(@user | Attachment | URL)',
 	category: 'Fun',
 	cooldown: 1000,
 	run: async function(message, args) {
 
-		let image = await this.utils.fetchImage(this.utils.getImagesFromMessage(message, args)[0]);
+		const images = this.utils.getImagesFromMessage(message, args);
+
+		if(images.length === 0) return this.commandHandler.invalidArguments(message);
+
+		let image = await this.utils.fetchImage(images[0]);
 		let gayPride = await this.jimp.read('./assets/gay.png');
 		gayPride = await gayPride.resize(image.bitmap.width, image.bitmap.height, this.jimp.RESIZE_BILINEAR);
 		image = await image.composite(gayPride, 0, 0);
