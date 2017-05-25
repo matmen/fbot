3e0d9136899d04947d0897467e48189f91d17d6a
matmen <gcmatmen@gmail.com>
2017-05-25 21:39:58 +0200

    Fix mentions in ascii command

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -8,8 +8,7 @@ module.exports = {
 
 		if(!argsString) return this.commandHandler.invalidArguments(message);
 
-
-		figlet(argsString, function(err, data) {
+		figlet(this.utils.filterMentions(argsString), function(err, data) {
 			if(err) return this.utils.handleCommandError(err);
 			if(data.length > 2000) return message.channel.send('The message you tried to convert is too long, try something shorter');
 
