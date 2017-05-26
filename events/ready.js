6ae8d86ef9cbd757f4afa5d337ccef7837772a4f
matmen <gcmatmen@gmail.com>
2017-05-26 08:10:23 +0200

    Update ready event to set the game text

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = function() {
 
 	this.client.on('ready', () => {
 		console.log(`${`[Shard ${this.client.shard.id}] [RUNNER]`.red} Ready for commands, serving ${this.client.guilds.size.toString().cyan} guilds`); // eslint-disable-line no-console
+		this.client.user.setGame(this.botCfg.messages.playing);
 	});
 
 };
