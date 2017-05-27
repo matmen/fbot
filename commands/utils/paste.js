188a6b25e554b6f9954b27befa838bb5b4f5f06f
matmen <gcmatmen@gmail.com>
2017-05-27 17:56:35 +0200

    Clean pastebin command

--- commands/utils/paste.js
+++ commands/utils/paste.js
@@ -1,36 +1,29 @@
 module.exports = {
-  description: 'Create a new pastebin',
-  category: 'Fun',
-  args: '[unlisted] (title) (text)',
-  cooldown: 1000,
-  run: async function(message, args) {
-    if (args.length === 0) return this.commandHandler.invalidArguments(message);
+	description: 'Create a new pastebin',
+	category: 'Fun',
+	aliases: ['pastebin'],
+	args: '[unlisted] (title) (text..)',
+	cooldown: 1000,
+	run: async function(message, args) {
+		if(args.length < 2) return this.commandHandler.invalidArguments(message);
 
-    let text, title, isPrivate = "0";
+		let unlisted = args[0] === 'true';
+		let title = args[unlisted ? 1 : 0];
+		let text = args.slice(unlisted ? 2 : 1).join(' ');
 
-    if (args[0].toLowerCase() == 'unlisted') {
-      title = args[1];
-      text = args.splice(2).join(' ')
-      isPrivate = "1";
-    } else {
-      title = args[0];
-      text = args.splice(1).join(' ')
-    };
-    
-    let reply = 'Creating Paste'
-    const apiKey = this.botCfg.pastebinApiKey
-    var msg = await message.channel.send(reply)
-    let url = "https://pastebin.com/api/api_post.php"
-    let response = await this.request(url, {
-      method: 'POST',
-      data: {
-        api_dev_key: apiKey,
-        api_paste_code: text,
-        api_paste_name: title,
-        api_paste_private: isPrivate,
-        api_option: "paste"
-      }
-    });
-    msg.edit((response.body.startsWith("Bad API request,") ? (":x: Error" + response.body.split("Bad API request,")[1]) : "Your paste URL is <"+response.body+">"))
-  }
-}
+		var msg = await message.channel.send('Creating paste..');
+
+		let response = await this.request('https://pastebin.com/api/api_post.php', {
+			method: 'POST',
+			data: {
+				api_dev_key: this.botCfg.pastebinApiKey,
+				api_paste_code: text,
+				api_paste_name: title,
+				api_paste_private: unlisted ? '1' : '0',
+				api_option: 'paste'
+			}
+		});
+
+		msg.edit(`Your paste has been created at <${response.body}>`);
+	}
+};
