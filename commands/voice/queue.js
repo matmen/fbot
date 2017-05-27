4f5cd28a664f2d1f8080ca827263e29515ce9880
Arze1 <Arze1@users.noreply.github.com>
2017-05-27 19:03:27 -0400

    Semicolons in queue.js

--- commands/voice/queue.js
+++ commands/voice/queue.js
@@ -4,15 +4,15 @@ module.exports = {
   args: 'NONE',
   cooldown: 1000,
   run: async function(message, args) {
-    let queue = this.songQueue.get(message.channel.id)
+    let queue = this.songQueue.get(message.channel.id);
     if (queue) {
-      let reply = "**SONG QUEUE**"
+      let reply = "**SONG QUEUE**";
       for(var x of queue) {
         reply += "\n#**"+queue.indexOf(x) + "** - `" + x.video.title + "` by `" + x.video.author + "` requested by `" + x.video.title + "`"
       }
-      message.channel.send(reply)
+      message.channel.send(reply);
     } else {
-      message.channel.send("You have no songs in your queue ;( add some with "+this.botCfg.prefix+"play")
+      message.channel.send("You have no songs in your queue ;( add some with "+this.botCfg.prefix+"play");
     }
   }
 }
