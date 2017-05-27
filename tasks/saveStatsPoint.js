37b4371afacd3e5327101ef0adc029ab139742fc
matmen <gcmatmen@gmail.com>
2017-05-27 16:21:52 +0200

    Add aimessages to stats table

--- tasks/saveStatsPoint.js
+++ tasks/saveStatsPoint.js
@@ -6,14 +6,16 @@ module.exports = {
 		let users = await this.shardManager.fetchClientValues('users.size'); // eslint-disable-line no-unused-vars
 		let messages = await this.db.query('SELECT COUNT(*) FROM messages'); // eslint-disable-line no-unused-vars
 		let commands = await this.db.query('SELECT COUNT(*) FROM COMMANDS'); // eslint-disable-line no-unused-vars
+		let aiMessages = await this.db.query('SELECT COUNT(*) FROM ai'); // eslint-disable-line no-unused-vars
 		let dbSize = await this.db.query('SELECT pg_database_size(\'fbot\') as size');
 		guilds = guilds.reduce((all, val) => all + val, 0);
 		channels = channels.reduce((all, val) => all + val, 0);
 		users = users.reduce((all, val) => all + val, 0);
 		messages = messages.rows[0].count;
+		aiMessages = aiMessages.rows[0].count;
 		commands = commands.rows[0].count;
 		dbSize = dbSize.rows[0].size;
 
-		this.db.query('INSERT INTO STATS VALUES ($1, $2, $3, $4, $5, $6, $7)', [guilds, channels, users, messages, commands, dbSize, Date.now()]);
+		this.db.query('INSERT INTO STATS VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [guilds, channels, users, messages, commands, aiMessages, dbSize, Date.now()]);
 	}
 };
