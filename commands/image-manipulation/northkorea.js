190307c293bcd6c007c9ecf48d5c9636d81728c7
matmen <gcmatmen@gmail.com>
2017-05-29 16:51:10 +0200

    Add northkorea command

---
+++ commands/image-manipulation/northkorea.js
@@ -0,0 +1,26 @@
+module.exports = {
+	description: 'Adds the north korean flag to the given argument',
+	args: '(@user | Attachment | URL)',
+	category: 'Fun',
+	cooldown: 5000,
+	run: async function(message, args) {
+
+		const images = this.utils.getImagesFromMessage(message, args);
+
+		if(images.length === 0) return this.commandHandler.invalidArguments(message);
+
+		let image = await this.utils.fetchImage(images[0]);
+		let overlay = await this.jimp.read('./assets/northkorea.png');
+		overlay = await overlay.resize(image.bitmap.width, image.bitmap.height, this.jimp.RESIZE_BILINEAR);
+		image = await image.composite(overlay, 0, 0);
+		image = await this.utils.getBufferFromJimp(image);
+
+		message.channel.send({
+			files: [{
+				attachment: image,
+				name: 'northkorea.png'
+			}]
+		});
+
+	}
+};
