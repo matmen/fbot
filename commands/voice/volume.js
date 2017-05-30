d0658c1f78d55a0258189680193275d18ff86648
matmen <gcmatmen@gmail.com>
2017-05-30 17:25:59 +0200

    Make volume command show the current volume if used with no arguments

--- commands/voice/volume.js
+++ commands/voice/volume.js
@@ -1,17 +1,20 @@
 module.exports = {
-	description: 'Sets the current playback volume',
+	description: 'Sets or shows the current playback volume',
 	category: 'Voice',
-	args: '(volume)',
+	args: '[volume]',
 	cooldown: 1000,
 	run: async function(message, args) {
-		if(args.length !== 1 || isNaN(args[0])) return this.commandHandler.invalidArguments(message);
 		if(!this.voiceStreams.has(message.guild.id) || !message.guild.members.get(this.client.user.id).voiceChannel) return message.channel.send(':x: The bot isn\'t playing anything!');
 
-		const volume = Math.max(0, Math.min(100, parseInt(args)));
+		let volume;
+		if(args.length === 1) {
+			volume = Math.max(0, Math.min(100, parseInt(args[0])));
+			this.voiceStreams.get(message.guild.id).setVolume(volume / 100);
+		} else {
+			volume = Math.round(this.voiceStreams.get(message.guild.id).volume * 100);
+		}
 
 		let emote = volume !== 0 ? volume >= 25 ? volume >= 75 ? ':loud_sound:' : ':sound:' : ':speaker:' : ':mute:';
-
-		this.voiceStreams.get(message.guild.id).setVolume(volume / 100);
-		message.channel.send(`${emote} Volume set to ${volume}%`);
+		message.channel.send(`${emote} Volume ${args.length === 1 ? 'is now set to' : 'is currently set to'} ${volume}%`);
 	}
 };
