11cd9b4c4189e7a5c68a3ef651e281b828872884
matmen <gcmatmen@gmail.com>
2017-06-01 23:08:02 +0200

    Remove git reset from update command

--- commands/admin-bot/update.js
+++ commands/admin-bot/update.js
@@ -7,7 +7,6 @@ module.exports = {
 
 		const startTime = Date.now();
 		const status = await message.channel.send(':clock1: Updating..');
-		this.childProcess.execSync('git reset --hard');
 		this.childProcess.execSync('git pull origin master');
 		this.commands = this.resourceLoader.loadCommands();
 		status.edit(`:white_check_mark: Done! Updated in \`${Date.now() - startTime}ms\`\nYou might want to restart all shards now. There are ${this.songQueues.size} active song queues.`);
