c361e7f9f7699f94638cc7d12db8ad5b0339fde0
matmen <gcmatmen@gmail.com>
2017-06-02 16:27:45 +0200

    Add song stats

--- commands/utils/stats.js
+++ commands/utils/stats.js
@@ -6,6 +6,7 @@ module.exports = {
 
 		const stats = await this.utils.queryDB('SELECT (SELECT count(*) FROM messages) messages, (SELECT count(*) FROM commands) commands');
 		const topCommandStats = await this.utils.queryDB('SELECT command,count(*) FROM commands GROUP BY 1 ORDER BY count(*) DESC LIMIT 1');
+		const topSongStats = await this.utils.queryDB('SELECT id,count(*) FROM songs GROUP BY 1 ORDER BY count(*) DESC LIMIT 1');
 
 		const messages = stats.rows[0].messages;
 		const commands = stats.rows[0].commands;
@@ -13,6 +14,11 @@ module.exports = {
 		const topCommand = {
 			command: topCommandStats.rows[0] && topCommandStats.rows[0].command,
 			uses: topCommandStats.rows[0] && topCommandStats.rows[0].count
+		};
+
+		const topSong = {
+			id: topSongStats.rows[0] && topSongStats.rows[0].id,
+			timesPlayed: topSongStats.rows[0] && topSongStats.rows[0].count
 		};
 
 		const shardGuilds = await this.client.shard.fetchClientValues('guilds.size');
@@ -25,7 +31,8 @@ module.exports = {
 		const modified = !!require('child_process').execSync('git status -s').toString();
 
 		let body = `Listening on ${guildCount} servers with ~${userCount} users\n\n`;
-		body += `Most used command: **${topCommand.command ? (this.botCfg.prefix + topCommand.command) : 'No commands used'}** (${topCommand.uses || 0} uses)\n\n`;
+		body += `Most used command: **${topCommand.command ? (this.botCfg.prefix + topCommand.command) : 'No commands used'}** (${topCommand.uses || 0} uses)\n`;
+		body += `Most played song: ${topSong.id ? `[Click here](https://youtube.com/watch?v=${topSong.id}) (Played ${topSong.timesPlayed} times)` : 'No songs played'}\n\n`;
 		body += `Commands used: **${commands}** in total\n`;
 		body += `Messages sent: **${messages}** in total\n\n`;
 		body += `Node ${process.version}\n`;
--- commands/utils/user.js
+++ commands/utils/user.js
@@ -11,6 +11,7 @@ module.exports = {
 
 		const stats = await this.utils.queryDB('SELECT (SELECT count(*) FROM messages WHERE userid = $1) messages, (SELECT count(*) FROM commands WHERE userid = $1) commands', [userID]);
 		const topCommandStats = await this.utils.queryDB('SELECT command,count(*) FROM commands WHERE userid = $1 GROUP BY 1 ORDER BY count(*) DESC LIMIT 1', [userID]);
+		const topSongStats = await this.utils.queryDB('SELECT id,count(*) FROM songs WHERE userid = $1 GROUP BY 1 ORDER BY count(*) DESC LIMIT 1', [userID]);
 
 		const messages = stats.rows[0].messages;
 		const commands = stats.rows[0].commands;
@@ -23,11 +24,17 @@ module.exports = {
 			uses: topCommandStats.rows[0] && topCommandStats.rows[0].count
 		};
 
+		const topSong = {
+			id: topSongStats.rows[0] && topSongStats.rows[0].id,
+			timesPlayed: topSongStats.rows[0] && topSongStats.rows[0].count
+		};
+
 		let body = `Seen on ${servers.size} servers (Shard ${this.client.shard.id}): `;
 		body += `\`${shownServers.map(s => s.name).join('`, `')} ${shownServers.length < servers.size ? `+ ${servers.size - shownServers.length} more` : ''}\`\n\n`;
-		body += `Most used command: **${topCommand.command ? (this.botCfg.prefix + topCommand.command) : 'No commands used'}** (${topCommand.uses || 0} uses)\n\n`;
+		body += `Most used command: **${topCommand.command ? (this.botCfg.prefix + topCommand.command) : 'No commands used'}** (${topCommand.uses || 0} uses)\n`;
+		body += `Most played song: ${topSong.id ? `[Click here](https://youtube.com/watch?v=${topSong.id}) (Played ${topSong.timesPlayed} times)` : 'No songs played'}\n\n`;
 		body += `Commands used: **${commands}** in total\n`;
-		body += `Messages sent: **${messages}** in total\n`;
+		body += `Messages sent: **${messages}** in total`;
 
 		const embed = new this.api.RichEmbed();
 
