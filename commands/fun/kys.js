6c37b2c694d00bab78198bb6d4048cb27d1a9c19
matmen <gcmatmen@gmail.com>
2017-06-06 21:38:20 +0200

    Add kys command

---
+++ commands/fun/kys.js
@@ -0,0 +1,22 @@
+module.exports = {
+	description: 'delet yourself',
+	category: 'Fun',
+	aliases: ['suicide'],
+	args: '[user]',
+	cooldown: 1000,
+	run: async function(message, args, argsString) {
+
+		const user = this.utils.filterMentions(argsString);
+
+		const hangman = '```\n_________\n' +
+			'|         |\n' +
+			`|         0 <-- ${user}\n` +
+			'|        /|\\\n' +
+			'|        / \\\n' +
+			'|\n' +
+			'|```';
+
+		message.channel.send(`${hangman}\n\nKill yourself, \`${user}\`\n<https://ropestore.org/?u=${encodeURI(user)}>`);
+
+	}
+};
