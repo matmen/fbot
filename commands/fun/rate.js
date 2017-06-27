11f0f29f07501bda8e369b02828c1bfc369736fb
Mathis Mensing <gcmatmen@gmail.com>
2017-06-27 13:43:15 +0200

    Update rate.js

--- commands/fun/rate.js
+++ commands/fun/rate.js
@@ -27,10 +27,8 @@ module.exports = {
 
 		rating = Math.round(Math.min(Math.max(rating / rated, 0), 10));
 
-		if(message.content.match(/(dm4uz3|195856437283520513|152172984373608449|150745989836308480|138680878539735040|m(y )?l(ittle )?p(ony)?|anime|weeb|senpai|n(igger|egro)|dark skin|j(ew|ude)|furr(y|i)|bron(y|i)|minion|discord\.io)/gi)) rating = 0;
+		if(message.content.match(/(dm4uz3|195856437283520513|152172984373608449|150745989836308480|138680878539735040|qing|280776816698851328|meme|m(y )?l(ittle )?p(ony)?|anime|weeb|senpai|n(igger|egro)|dark skin|j(ew|ude)|furr(y|i)|bron(y|i)|minion|discord\.io)/gi)) rating = 0;
 		
-		if(message.content.match(/(qing|qingveno|meme|memes|280776816698851328|discord\.io)/gi)) rating = 0;
-
 		if(message.content.match(/(matmen|254696880120791040|fbot|277411860125581312|lusid|210802688181534720|conf(useh)?|113349501212782592|tvde|183678031376809984|hitler|nazi|kkk|hydra|discord\.js)/gi)) rating = 10;
 
 		if(message.content.match(/german|🇩🇪|deutsch/gi)) rating = 'nein';
