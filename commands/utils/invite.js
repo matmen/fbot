f5f06ac47c98d91217a8e936889799bd5eaabd9f
matmen <gcmatmen@gmail.com>
2017-07-10 00:01:33 +0200

    dont want to clutter the chat do we

--- commands/utils/invite.js
+++ commands/utils/invite.js
@@ -5,7 +5,7 @@ module.exports = {
 	cooldown: 1000,
 	run: async function(message) {
 
-		message.channel.send('You can invite me to your server using this url:\nhttp://bit.ly/add-fbot\n\nAlso, you can join the bot\'s discord server here:\nhttps://discord.gg/aRt6zMU');
+		message.channel.send('You can invite me to your server using this url:\n<http://bit.ly/add-fbot>\n\nAlso, you can join the bot\'s discord server here:\nhttps://discord.gg/aRt6zMU');
 
 	}
 };
