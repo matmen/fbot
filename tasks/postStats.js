805700c80ca2ffb5e2d54ce40cdd9843798c9a66
matmen <gcmatmen@gmail.com>
2017-07-11 13:33:53 +0200

    stats collecting for discordbots.org

---
+++ tasks/postStats.js
@@ -0,0 +1,16 @@
+const botCfg = require('../configs/bot.json');
+const request = require('snekfetch');
+
+module.exports = {
+	interval: 10 * 60 * 1000,
+	run: async function () {
+		let guilds = await this.shardManager.fetchClientValues('guilds.size');
+
+		request.post(`https://discordbots.org/api/bots/${botCfg.discordbotsID}/stats`)
+			.set('Authorization', botCfg.discordbotsApiKey)
+			.send({
+				shards: guilds
+			})
+			.then(r => r, e => e);
+	}
+};
