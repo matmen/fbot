96ac06881b646a61b07cfffce1258d536d893225
matmen <gcmatmen@gmail.com>
2017-07-13 21:34:12 +0200

    fix

--- commands/fun/xd.js
+++ commands/fun/xd.js
@@ -15,7 +15,7 @@ module.exports = {
 		let reply = a + s(11) + a + s(5) + b + s(2) + c + d +
 			s(2) + a + s(7) + a + s(7) + b + s(5) + c + d +
 			s(4) + a + s(3) + a + s(9) + b + s(6) + c + d +
-			s(6) + a + s(11) + s(Math.round(a.length / 2 - 1)) + b + s(6) + c + d +
+			s(6) + s(Math.round(a.length / 2 - 1)) + a + s(11) + s(Math.round(a.length / 2)) + b + s(6) + c + d +
 			s(4) + a + s(3) + a + s(9) + b + s(6) + c + d +
 			s(2) + a + s(7) + a + s(7) + b + s(5) + c + d +
 			a + s(11) + a + s(5) + b + s(2) + c;
