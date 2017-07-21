2a063dc9f4a0766ff20c16e5ffdf42cb9a4622a7
matmen <gcmatmen@gmail.com>
2017-07-21 20:07:17 +0200

    right

--- commands/image-manipulation/composite.js
+++ commands/image-manipulation/composite.js
@@ -12,7 +12,7 @@ module.exports = {
 		let result;
 		for (let index = 0; index < images.length; index++) {
 			let image = await this.utils.fetchImage(images[index]);
-			if (!image) continue;
+			if (!image || image instanceof Error) continue;
 
 			if (!result) result = new this.jimp(image.bitmap.width, image.bitmap.height, 0xFFFFFFFF);
 
