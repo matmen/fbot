7f806750755f414dcaade342350f3a30e603d931
Tim van den Essen <tim3345@live.nl>
2017-07-23 14:24:56 +0200

    changed bad indentation

--- commands/image-manipulation/stock.js
+++ commands/image-manipulation/stock.js
@@ -11,9 +11,9 @@ module.exports = {
 		let image = await this.utils.fetchImage(images[0]);
 		if (image instanceof Error) return this.utils.handleCommandError(image, message);
 
-        let overlay = await this.jimp.read('./assets/stock.png');
-        overlay = await overlay.resize(image.bitmap.width, image.bitmap.height);
-        image = await image.composite(overlay, 0, 0);
+                let overlay = await this.jimp.read('./assets/stock.png');
+                overlay = await overlay.resize(image.bitmap.width, image.bitmap.height);
+                image = await image.composite(overlay, 0, 0);
 
 		image = await this.utils.getBufferFromJimp(image);
 
