7f46fcf44b26952c1eee24bc2ab34ce88a78d670
Tim van den Essen <tim3345@live.nl>
2017-07-23 14:21:57 +0200

    added stock command

---
+++ commands/image-manipulation/stock.js
@@ -0,0 +1,27 @@
+module.exports = {
+	description: 'Adds the shutterstock overlay to an image.',
+	args: '(@user | Attachment | URL)',
+	category: 'Fun',
+	cooldown: 5000,
+	run: async function (message, args) {
+		const images = await this.utils.getImagesFromMessage(message, args);
+
+		if (images.length === 0) return this.commandHandler.invalidArguments(message);
+
+		let image = await this.utils.fetchImage(images[0]);
+		if (image instanceof Error) return this.utils.handleCommandError(image, message);
+
+        let overlay = await this.jimp.read('./assets/stock.png');
+        overlay = await overlay.resize(image.bitmap.width, image.bitmap.height);
+        image = await image.composite(overlay, 0, 0);
+
+		image = await this.utils.getBufferFromJimp(image);
+
+		message.channel.send({
+			files: [{
+				attachment: image,
+				name: 'stock.png'
+			}]
+		});
+	}
+};
