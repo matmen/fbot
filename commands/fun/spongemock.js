b114abeb4970d453f8a0bda33cf1190b16116fd7
matmen <gcmatmen@gmail.com>
2017-07-24 22:16:56 +0200

    eslint :weary:

--- commands/fun/spongemock.js
+++ commands/fun/spongemock.js
@@ -1,15 +1,15 @@
 module.exports = {
 	description: 'tRAnSfOrMs tExT iNto sPoNgEmOcK fOrMaT.',
-  	aliases: ['mock'],
+	aliases: ['mock'],
 	category: 'Fun',
 	args: '(text..)',
 	cooldown: 1000,
-	run: async function(message, args, argsString) {
-		if(!argsString) return this.commandHandler.invalidArguments(message);
-    
-    		let text = [];
+	run: async function (message, args, argsString) {
+		if (!argsString) return this.commandHandler.invalidArguments(message);
 
-		for(const char of this.utils.filterMentions(argsString).split(''))
+		let text = [];
+
+		for (const char of this.utils.filterMentions(argsString).split(''))
 			text.push(Math.random() >= 0.5 ? char.toUpperCase() : char.toLowerCase());
 
 		message.channel.send(text.join(''));
