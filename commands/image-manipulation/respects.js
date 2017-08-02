b5b60f2bb624c8a9ea58f2431fbc0ad5c8aa9719
matmen <gcmatmen@gmail.com>
2017-08-02 22:34:32 +0200

    Add respects command

---
+++ commands/image-manipulation/respects.js
@@ -0,0 +1,22 @@
+module.exports = {
+	description: 'Press F to pay respects',
+	args: '(@user | Attachment | URL)',
+	category: 'Fun',
+	cooldown: 5000,
+	run: async function (message, args) {
+		const images = await this.utils.getImagesFromMessage(message, args);
+
+		if (images.length === 0) return this.commandHandler.invalidArguments(message);
+
+		const image = await this.utils.fetchFromAPI('respects', {
+			images
+		});
+
+		message.channel.send({
+			files: [{
+				attachment: image,
+				name: 'respects.png'
+			}]
+		});
+	}
+};
