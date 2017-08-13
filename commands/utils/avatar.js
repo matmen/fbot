c1655bc8a420b8be84aad529c5daee1b5425ffeb
matmen <gcmatmen@gmail.com>
2017-08-13 20:07:54 +0200

    Make f!avatar work without mentions/IDs

--- commands/utils/avatar.js
+++ commands/utils/avatar.js
@@ -3,15 +3,24 @@ module.exports = {
 	category: 'Utils',
 	args: '[@user]',
 	cooldown: 1000,
-	run: async function (message, args) {
-		let id = args[0];
-		if (!id) id = message.author.id;
+	run: async function (message, args, argsString) {
+		let user = message.author;
 
-		if (!/^(<@!?)?\d+>?$/.test(id)) return this.commandHandler.invalidArguments(message);
-		id = id.replace(/[^\d]/g, '');
+		if (argsString) {
+			const match = message.guild.members.filter(member => {
+				if (member.user.tag.toLowerCase().includes(argsString.toLowerCase())) return true;
+				if (member.nickname && member.nickname.toLowerCase().includes(argsString.toLowerCase())) return true;
+				if (member.user.id === argsString.replace(/[^\d]/g, '')) return true;
+				return false;
+			}).sort((m1, m2) => {
+				const m1Time = m1.lastMessage && m1.lastMessage.createdTimestamp || 0;
+				const m2Time = m2.lastMessage && m2.lastMessage.createdTimestamp || 0;
 
-		if (!this.client.users.has(id)) return message.channel.send('The requested user could not be found');
-		const user = this.client.users.get(id);
+				return m2Time - m1Time;
+			}).first();
+
+			if (match) user = match.user;
+		}
 
 		message.channel.send(`\`${user.tag}\`${user.avatar ? `'s avatar is ${user.avatarURL({format: user.avatar.startsWith('a_') ? 'gif' : 'png', size: 2048})}` : ' does not have an avatar set'}`);
 	}
