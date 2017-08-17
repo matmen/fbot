a8f805f3b31e14db37812b8c084ed7c7ec5ef043
matmen <gcmatmen@gmail.com>
2017-08-17 16:32:39 +0200

    Remove last AI leftovers, fix stats collection

--- constructors/createTables.js
+++ constructors/createTables.js
@@ -1,7 +1,7 @@
 module.exports = async function (db) {
 	await db.query('CREATE TABLE IF NOT EXISTS commands ("id" BIGINT PRIMARY KEY NOT NULL, "command" TEXT NOT NULL, "userid" BIGINT NOT NULL, "channelid" BIGINT NOT NULL, "serverid" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS messages ("id" BIGINT PRIMARY KEY NOT NULL, "userid" BIGINT NOT NULL, "channelid" BIGINT NOT NULL, "serverid" BIGINT NOT NULL)');
-	await db.query('CREATE TABLE IF NOT EXISTS stats ("servers" BIGINT NOT NULL, "channels" BIGINT NOT NULL, "users" BIGINT NOT NULL, "messages" BIGINT NOT NULL, "commands" BIGINT NOT NULL, "aimessages" BIGINT NOT NULL, "dbsize" BIGINT NOT NULL, "time" BIGINT NOT NULL)');
+	await db.query('CREATE TABLE IF NOT EXISTS stats ("servers" BIGINT NOT NULL, "channels" BIGINT NOT NULL, "users" BIGINT NOT NULL, "messages" BIGINT NOT NULL, "commands" BIGINT NOT NULL, "dbsize" BIGINT NOT NULL, "time" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS settings ("server" BIGINT NOT NULL, "setting" TEXT NOT NULL, "value" TEXT)');
 	await db.query('CREATE TABLE IF NOT EXISTS blacklists ("type" TEXT NOT NULL, "id" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS songs ("id" TEXT NOT NULL, "userid" BIGINT NOT NULL)');
--- tasks/saveStatsPoint.js
+++ tasks/saveStatsPoint.js
@@ -2,20 +2,18 @@ module.exports = {
 	interval: 30 * 60 * 1000,
 	run: async function () {
 		let guilds = await this.shardManager.fetchClientValues('guilds.size');
-		let channels = await this.shardManager.broadcastEval('this.guilds.map(g => g.memberCount).reduce((a, c) => a + c, 0)'); // eslint-disable-line no-unused-vars
-		let users = await this.shardManager.fetchClientValues('users.size'); // eslint-disable-line no-unused-vars
-		let messages = await this.db.query('SELECT COUNT(*) FROM messages'); // eslint-disable-line no-unused-vars
-		let commands = await this.db.query('SELECT COUNT(*) FROM COMMANDS'); // eslint-disable-line no-unused-vars
-		let aiMessages = await this.db.query('SELECT COUNT(*) FROM ai'); // eslint-disable-line no-unused-vars
+		let channels = await this.shardManager.fetchClientValues('channels.size');
+		let users = await this.shardManager.fetchClientValues('users.size');
+		let messages = await this.db.query('SELECT COUNT(*) FROM messages');
+		let commands = await this.db.query('SELECT COUNT(*) FROM COMMANDS');
 		let dbSize = await this.db.query('SELECT pg_database_size(\'fbot\') as size');
 		guilds = guilds.reduce((all, val) => all + val, 0);
 		channels = channels.reduce((all, val) => all + val, 0);
 		users = users.reduce((all, val) => all + val, 0);
 		messages = messages.rows[0].count;
-		aiMessages = aiMessages.rows[0].count;
 		commands = commands.rows[0].count;
 		dbSize = dbSize.rows[0].size;
 
-		this.db.query('INSERT INTO STATS VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [guilds, channels, users, messages, commands, aiMessages, dbSize, Date.now()]);
+		this.db.query('INSERT INTO STATS VALUES ($1, $2, $3, $4, $5, $6, $7)', [guilds, channels, users, messages, commands, dbSize, Date.now()]);
 	}
 };
