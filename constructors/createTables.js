22ae9a72980c8bf96c2bb50076105fead1788907
matmen <gcmatmen@gmail.com>
2017-08-19 14:36:34 +0200

    Make command (message) IDs not unique

--- constructors/createTables.js
+++ constructors/createTables.js
@@ -1,5 +1,5 @@
 module.exports = async function (db) {
-	await db.query('CREATE TABLE IF NOT EXISTS commands ("id" BIGINT PRIMARY KEY NOT NULL, "command" TEXT NOT NULL, "userid" BIGINT NOT NULL, "channelid" BIGINT NOT NULL, "serverid" BIGINT NOT NULL)');
+	await db.query('CREATE TABLE IF NOT EXISTS commands ("id" BIGINT NOT NULL, "command" TEXT NOT NULL, "userid" BIGINT NOT NULL, "channelid" BIGINT NOT NULL, "serverid" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS messages ("id" BIGINT PRIMARY KEY NOT NULL, "userid" BIGINT NOT NULL, "channelid" BIGINT NOT NULL, "serverid" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS stats ("servers" BIGINT NOT NULL, "channels" BIGINT NOT NULL, "users" BIGINT NOT NULL, "messages" BIGINT NOT NULL, "commands" BIGINT NOT NULL, "dbsize" BIGINT NOT NULL, "time" BIGINT NOT NULL)');
 	await db.query('CREATE TABLE IF NOT EXISTS settings ("server" BIGINT NOT NULL, "setting" TEXT NOT NULL, "value" TEXT)');
