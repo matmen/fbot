0494f3125acdc233bfa5b810a6b20ecbbbca67d1
matmen <gcmatmen@gmail.com>
2017-08-28 14:34:51 +0200

    Move screenshot to API

--- commands/utils/screenshot.js
+++ commands/utils/screenshot.js
@@ -1,8 +1,3 @@
-const headers = new Map([
-	['Accept-Language', 'en-US'],
-	['Content-Language', 'en-US']
-]);
-
 module.exports = {
 	description: 'Takes a screenshot of a web page',
 	category: 'Utils',
@@ -11,29 +6,16 @@ module.exports = {
 	cooldown: 3000,
 	run: async function (message, args, argsString) {
 		if (!argsString) return this.commandHandler.invalidArguments(message);
-		if (!/^https?:\/\//.test(argsString)) argsString = `http://${argsString}`;
 
-		const browser = await this.puppeteer.launch({
-			args: ['--no-sandbox'],
-			ignoreHTTPSErrors: true
+		const image = await this.utils.fetchFromAPI('screenshot', {
+			args: {
+				text: argsString
+			}
 		});
-
-		setTimeout(() => {
-			browser.close();
-		}, 30000);
-
-		const page = await browser.newPage();
-		await page.setJavaScriptEnabled(false);
-		page.setExtraHTTPHeaders(headers);
-
-		await page.goto(argsString);
-		const result = await page.screenshot();
-
-		browser.close();
 
 		message.channel.send({
 			files: [{
-				attachment: result,
+				attachment: image,
 				name: 'screenshot.png'
 			}]
 		});
