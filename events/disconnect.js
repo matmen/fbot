4e958cf5d4ab0c78ec95462ed4d86b67723ca90a
matmen <gcmatmen@gmail.com>
2017-09-06 19:10:10 +0200

    thanks, discord

--- events/disconnect.js
+++ events/disconnect.js
@@ -1,7 +1,8 @@
 module.exports = function () {
 
 	this.client.on('disconnect', (closeEvent) => {
-		console.log(`${`[Shard ${this.client.shard.id}] [RUNNER]`.red} Connection lost with code ${closeEvent.code} (${closeEvent.reason || 'No message supplied'})`); // eslint-disable-line no-console
+		console.log(`${`[Shard ${this.client.shard.id}] [RUNNER]`.red} Connection closed with code ${closeEvent.code} (${closeEvent.reason || 'No message supplied'})`); // eslint-disable-line no-console
+		process.exit();
 	});
 
 };
