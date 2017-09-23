ee596c4ed93b4d9f692474cc723b2722adb51817
matmen <gcmatmen@gmail.com>
2017-09-23 16:38:16 +0200

    owo

--- commands/image-manipulation/hacker.js
+++ commands/image-manipulation/hacker.js
@@ -1,15 +1,19 @@
 module.exports = {
 	description: 'Hacking in progress..',
-	args: '(text..)',
+	args: '[variation] (text..)',
 	category: 'Fun',
 	aliases: ['hack'],
 	cooldown: 5000,
 	run: async function (message, args, argsString) {
 		if (!argsString) return this.commandHandler.invalidArguments(message);
 
+		const text = /\d+/.test(args[0]) ? args.slice(1, args.length).join(' ') : argsString;
+		const variation = /\d+/.test(args[0]) ? args[0] : 1;
+
 		const image = await this.utils.fetchFromAPI('hacker', {
 			args: {
-				text: this.utils.filterMentions(argsString)
+				text: this.utils.filterMentions(text),
+				variation
 			}
 		});
 
