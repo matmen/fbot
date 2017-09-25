5da2517803d2a41a9a9c699d8cab241eaed9e703
matmen <gcmatmen@gmail.com>
2017-09-25 17:21:45 +0200

    GabeN

---
+++ commands/image-manipulation/gaben.js
@@ -0,0 +1,22 @@
+module.exports = {
+	description: 'our mighty lord GabeN',
+	args: '(@user | Attachment | URL)',
+	category: 'Fun',
+	cooldown: 5000,
+	run: async function (message, args) {
+		const images = await this.utils.getImagesFromMessage(message, args);
+
+		if (images.length === 0) return this.commandHandler.invalidArguments(message);
+
+		const image = await this.utils.fetchFromAPI('gaben', {
+			images
+		});
+
+		message.channel.send({
+			files: [{
+				attachment: image,
+				name: 'gay.png'
+			}]
+		});
+	}
+};
