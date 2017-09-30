ac623e5ea56836c8c2f5300cf84e31deceec6b59
matmen <gcmatmen@gmail.com>
2017-09-30 17:11:13 +0200

    setActivity

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = function () {
 
 	this.client.once('ready', () => {
 		console.log(`${`[Shard ${this.client.shard.id}] [RUNNER]`.red} Ready for commands, serving ${this.client.guilds.size.toString().cyan} guilds`); // eslint-disable-line no-console
-		this.client.user.setGame(this.botCfg.messages.playing);
+		this.client.user.setActivity(this.botCfg.messages.playing);
 	});
 
 };
