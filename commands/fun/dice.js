c8ba912c8c2a8099f7608cfdbbecff2e47a723f5
Yusuf R <notYusufR@gmail.com>
2017-10-18 20:15:27 +0300

    Update dice.js

--- commands/fun/dice.js
+++ commands/fun/dice.js
@@ -4,6 +4,29 @@ module.exports = {
 	aliases: ['rolldice', 'rolladice', '1to6', 'onetosix', '126'],
 	cooldown: 1000,
 	run: async function (message) {
-		// Roll the dice
+		message.channel.startTyping();
+        
+		const dice = Math.floor(Math.random() * 6);
+
+		if (dice === 0) {
+		    message.channel.send('You rolled...:one:!');
+		}
+		if (dice === 1) {
+		    message.channel.send('You rolled...:two:!');
+		}
+		if (dice === 2) {
+		    message.channel.send('You rolled...:three:!');
+		}
+		if (dice === 3) {
+		    message.channel.send('You rolled...:four:!');
+		}
+		if (dice === 4) {
+		    message.channel.send('You rolled...:five:!');
+		}
+		if (dice === 5) {
+		    message.channel.send('You rolled...:six:!');
+		}
+
+		message.channel.stopTyping();
 	}
 };
