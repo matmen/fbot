42cc28bc93e43a9b7a9c6647a2137fe9e88e00c0
Yusuf R <notYusufR@gmail.com>
2017-10-18 17:14:36 +0300

    Create coin.js

---
+++ commands/fun/coin.js
@@ -0,0 +1,9 @@
+module.exports = {
+	description: 'Flips a coin. You can get get head or tails, or the coin might just keep rolling forever 😟',
+	category: 'Fun',
+	aliases: ['flipcoin', 'flipacoin', 'headsortails'],
+	cooldown: 1000,
+	run: async function (message) {
+		
+	}
+};
