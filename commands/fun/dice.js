209df3cbfb85c9b598e624045bdb93d9f68734e0
Yusuf R <notYusufR@gmail.com>
2017-10-18 19:47:09 +0300

    Create dice.js

---
+++ commands/fun/dice.js
@@ -0,0 +1,9 @@
+module.exports = {
+	description: 'Rolls a six-sided dice',
+	category: 'Fun',
+	aliases: ['rolldice', 'rolladice', '1to6', 'onetosix', '126'],
+	cooldown: 1000,
+	run: async function (message) {
+		// Roll the dice
+	}
+};
