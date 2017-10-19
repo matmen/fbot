6f3db03bd50b5935927a33f58d403ae759bfc048
Mathis Mensing <gcmatmen@gmail.com>
2017-10-19 19:48:01 +0200

    Switch to ternary expression

--- commands/fun/coin.js
+++ commands/fun/coin.js
@@ -4,12 +4,6 @@ module.exports = {
 	aliases: ['flipcoin', 'flipacoin', 'headsortails'],
 	cooldown: 1000,
 	run: async function (message) {
-		const coin = Math.floor(Math.random() * 2);
-        
-		  if (coin === 0) {
-		      message.channel.send('You flipped...**heads**!');
-		  } else if (coin === 1) {
-		    message.channel.send('You flipped...**tails**!');
-		  }
+		  message.channel.send(`You flipped...**${Math.random() > 0.5 ? 'heads' : 'tails'}**!`);
 	}
 };
