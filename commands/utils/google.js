bd8f2d2388fb47a328265cf23ac873dfe95c8458
matmen <gcmatmen@gmail.com>
2017-10-21 14:21:01 +0200

    f!google

---
+++ commands/utils/google.js
@@ -0,0 +1,34 @@
+module.exports = {
+	description: 'Returns the results of a google search',
+	category: 'Utils',
+	args: '(query..)',
+	aliases: ['g', 'search'],
+	cooldown: 3000,
+	run: async function (message, args, argsString) {
+		if (!argsString) return this.commandHandler.invalidArguments(message);
+
+		const resultRequest = await this.utils.fetchFromAPI('google', {
+			args: {
+				text: argsString
+			}
+		});
+
+		const results = JSON.parse(resultRequest);
+		const embed = new this.api.MessageEmbed();
+
+		embed.setTitle('Google Search Results');
+
+		embed.setAuthor(message.author.tag, message.author.displayAvatarURL({
+			format: 'png',
+			size: 2048
+		}));
+
+		for (const result of results.data.slice(0, 4)) {
+			embed.addField(result.title, result.link);
+		}
+
+		message.channel.send({
+			embed
+		});
+	}
+};
